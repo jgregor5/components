da284b0cc1307c4aa5d1b5e3881e4688eae74332
julian <julian.gregorio@adhocware.com>
2019-02-07 07:21:11 +0100

    info.sendevent command for testing/debugging components

--- src/implement/InfoComponent.java
+++ src/implement/InfoComponent.java
@@ -3,14 +3,29 @@ package implement;
 
 import commander.ComponentManager;
 import commander.IComponent;
+import commander.IEventListener;
+import commander.IEventSource;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.json.JSONObject;
 
 /**
  *
  * @author julian
  */
-public class InfoComponent implements IComponent {
+public class InfoComponent implements IComponent, IEventSource {
+    
+    private static final Logger LOGGER = Logger.getLogger(InfoComponent.class.getName());
 
+    private final Set<IEventListener> listeners;
+    
+    public InfoComponent() {
+        this.listeners = new HashSet<>();
+    }
+    
     @Override
     public String getName() {
         return "info";
@@ -19,15 +34,58 @@ public class InfoComponent implements IComponent {
     @Override
     public String[] getCommands() {
         return new String[]{
-            "info.components"
+            "info.components", "info.sendevent"
         };
     }
 
     @Override
-    public JSONObject execute(JSONObject command) {        
-        return new JSONObject().
-            put("sucess", true).
-            put("items", ComponentManager.getInstance().getComponentsInfo());
+    public JSONObject execute(JSONObject command) {       
+        
+        JSONObject result = new JSONObject();
+        
+        switch (command.getString("command")) {
+            
+            case "info.components": 
+                result.put("sucess", true).
+                put("items", ComponentManager.getInstance().getComponentsInfo());
+                break;
+                
+            case "info.sendevent":                
+                JSONObject event = new JSONObject();
+                event.put("source", command.getString("source"));
+                event.put("type", command.getString("type"));                
+                Set<String> ignoreSet = new HashSet<>(Arrays.asList(
+                        new String[]{"command", "source", "type"}));                
+                
+                for (String name: JSONObject.getNames(command)) {
+                    if (!ignoreSet.contains(name)) {
+                        event.put(name, command.get(name));
+                    }
+                }
+                sendEvent(event);
+                result.put("success", true);
+                break;
+        }
+        
+        return result;        
     }
+
+    private void sendEvent(JSONObject jo) {        
+        for (IEventListener listener: this.listeners) {
+            listener.handleEvent(jo);
+        }
+    }
+    
+    @Override
+    public void registerListener(IEventListener el) {
+        this.listeners.add(el);
+        LOGGER.log(Level.CONFIG, "registered {0}", el.getClass().getName());
+    }
+
+    @Override
+    public void unregisterListener(IEventListener el) {
+        this.listeners.remove(el);
+        LOGGER.log(Level.CONFIG, "unregistered {0}", el.getClass().getName());
+    }   
     
 }
